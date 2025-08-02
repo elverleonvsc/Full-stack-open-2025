52bcc2889d748b394ecabee05a65c224a3900109
unknown <Elverejemplo@gmail.com>
2025-08-02 22:25:45 +0100

    first steps into backend

---
+++ part03/PhoneBook-Backend-step1/index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+
+const app = express()
+
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+
+const time = new Date()
+
+const total = persons.length
+
+ 
+ 
+
+
+
+
+
+
+
+app.get('/', (resquest,responde) => {
+    responde.send('Hello backend, elver')
+
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+  if (person) {
+ res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req,res) => {
+  const id = req.params.id
+  personDelete = persons.filter(p => p.id !== id)
+  res.json(personDelete)
+})
+
+const generateId = ()=>{
+   return Math.random() * 10000
+  
+}
+
+
+
+
+app.post('/api/persons' ,(req,res)=> {
+  const body = req.body
+
+  const sameName = persons.find(p=> p.name === body.name)
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number is missing' })
+  }
+  else if (sameName){
+    return res.status(400).json({ error: 'name must be unique'})
+   
+  
+  } else {
+ const person = {
+    name:body.name,
+    number:body.number,
+    important:body.important,
+    id:generateId()
+  }
+  persons = persons.concat(person)
+  res.json(person)
+  }
+ 
+})
+
+app.get('/info', (req,res) => {
+  res.send(`
+    <p>Phonebook has in for ${total} people</p>
+    <p>${time}</p>
+    `)
+})
+
+
+const port = 3001
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    
+})
