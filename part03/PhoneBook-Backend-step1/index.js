93174e97c2073fddeda6345d82b8ac23fa39b805
unknown <Elverejemplo@gmail.com>
2025-08-06 22:50:53 +0100

    Upload backend changes for Render deployment

--- part03/PhoneBook-Backend-step1/index.js
+++ part03/PhoneBook-Backend-step1/index.js
@@ -159,6 +159,6 @@ app.get('/info', (req,res) => {
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${PORT}`);
     
 })
